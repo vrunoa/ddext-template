0e915e981464a6fd8d84ec484786b5d242d2b4ca
vrunoa <vruno.alassia@gmail.com>
2022-12-19 10:06:51 -0800

    remove static

--- ui/src/desktop.ts
+++ ui/src/desktop.ts
@@ -3,15 +3,15 @@ import { createDockerDesktopClient } from '@docker/extension-api-client';
 const client = createDockerDesktopClient();
 
 export default class DesktopClientHelper {
-  static openUrl(url: string): void {
+  openUrl(url: string): void {
     client.host.openExternal(url);
   }
 
-  static get(route: string) {
+  get(route: string) {
     return client.extension.vm?.service?.get(route);
   }
 
-  static toast(msg: string): void {
+  toast(msg: string): void {
     client.desktopUI.toast.error(msg);
   }
 }
